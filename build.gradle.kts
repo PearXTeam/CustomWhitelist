dd7b9b6967918f40246d7aa80efb8fc627a14b27
mrAppleXZ <mr.applexz@gmail.com>
2018-07-26 13:15:14 +0500

    The buildscript, .gitignore

---
+++ build.gradle.kts
@@ -0,0 +1,41 @@
+import net.minecraftforge.gradle.common.BaseExtension
+import net.minecraftforge.gradle.user.UserBaseExtension
+
+buildscript {
+    val fg_version: String by project
+    repositories {
+        jcenter()
+        maven { url = uri("http://files.minecraftforge.net/maven") }
+    }
+    dependencies {
+       classpath("net.minecraftforge.gradle:ForgeGradle:$fg_version")
+    }
+}
+
+val mod_version: String by project
+val mod_build_number: String by project
+val forge_version: String by project
+val mappings_version: String by project
+val mc_version: String by project
+val accepted_mc_versions: String by project
+
+apply(plugin = "net.minecraftforge.gradle.forge")
+
+version = "$mod_version+$mod_build_number"
+group = "ru.pearx.customwhitelist"
+
+configure<BasePluginConvention> {
+    archivesBaseName = "customwhitelist-$mc_version"
+}
+configure<JavaPluginConvention> {
+    sourceCompatibility = JavaVersion.VERSION_1_8
+}
+configure<UserBaseExtension> {
+    version = "$mc_version-$forge_version"
+    runDir = "run"
+    mappings = mappings_version
+    replace("@VERSION@", mod_version)
+    replace("@MCVERSION@", mc_version)
+    replace("acceptedMinecraftVersions = \"\"", "acceptedMinecraftVersions = \"$accepted_mc_versions\"")
+    replaceIn("CustomWhitelist.java")
+}
